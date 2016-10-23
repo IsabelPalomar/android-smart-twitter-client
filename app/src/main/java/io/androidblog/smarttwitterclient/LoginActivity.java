c522c2d211461cb733019b96f70af97b657549c1
Isabel Palomar <isabel.palomarb@gmail.com>
2016-10-23 15:12:10 -0400

    Login UI
    Concepts:
    * New Strings
        * Supporting Different Languages
    * New dimensions
    * New colors
    * Drawable resources for different dimensions
    * activity_login change

--- app/src/main/java/io/androidblog/smarttwitterclient/LoginActivity.java
+++ app/src/main/java/io/androidblog/smarttwitterclient/LoginActivity.java
@@ -7,7 +7,6 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme_NightMode);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
