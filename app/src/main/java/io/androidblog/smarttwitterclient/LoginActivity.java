ab66181e2842870c77916b4e84cd21c05555fe08
Isabel Palomar <isabel.palomarb@gmail.com>
2016-10-23 18:54:47 -0400

    Timeline UI - Part 1, DrawerLayout
    Concepts:
    *Intents, onClickEvents
    *DrawerLayout
    *NavigationView
    *Menus (groups and items)
    *CoordinatorLayout
    *Toolbar
    *FloatingActionButton
    *ActionBarDrawerToggle
    
    Libraries
    * Butterknife, zelezni plugin

--- app/src/main/java/io/androidblog/smarttwitterclient/LoginActivity.java
+++ app/src/main/java/io/androidblog/smarttwitterclient/LoginActivity.java
@@ -1,7 +1,11 @@
 package io.androidblog.smarttwitterclient;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void onClick() {
+        Intent i = new Intent(this, TimelineActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/io/androidblog/smarttwitterclient/TimelineActivity.java
@@ -0,0 +1,78 @@
+package io.androidblog.smarttwitterclient;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class TimelineActivity extends AppCompatActivity {
+
+    ActionBarDrawerToggle drawerToggle;
+    @BindView(R.id.ndTweets)
+    DrawerLayout ndTweets;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        drawerToggle = setupDrawerToggle();
+        ndTweets.addDrawerListener(drawerToggle);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    // `onPostCreate` called when activity start-up is complete after `onStart()`
+    // NOTE! Make sure to override the method with only a single `Bundle` argument
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, ndTweets, toolbar, R.string.drawer_open, R.string.drawer_close);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.ndTweets)
+    public void onClick() {
+    }
+}
